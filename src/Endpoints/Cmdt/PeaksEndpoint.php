2c27c1bc462dea319806e15f78a80de39b8a0b1e
Vojtěch Sedláček <sedlacekvojtech@gmail.com>
2021-01-20 14:24:07 +0100

    latest

--- src/Endpoints/Cmdt/PeaksEndpoint.php
+++ src/Endpoints/Cmdt/PeaksEndpoint.php
@@ -6,4 +6,11 @@ use EnergyHub\ApiClient\Endpoints\BaseEndpoint;
 class PeaksEndpoint extends BaseEndpoint
 {
     protected $endpoint = 'cmdt/peaks';
+
+	public function latest(): PeaksEndpoint
+	{
+		$this->endpoint .= '/latest';
+
+		return $this;
+	}
 }
